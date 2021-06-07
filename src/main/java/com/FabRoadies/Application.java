a042f7cde0019efbb43b89b547b40989ea13a2ba
shailendra singh <shailendrasingh9649933029@gmail.com>
2021-06-07 19:03:00 +0530

    Second update

--- src/main/java/com/FabRoadies/Application.java
+++ src/main/java/com/FabRoadies/Application.java
@@ -8,7 +8,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		System.out.println("Shailendra");
+		System.out.println("Shailendra singh rathore");
 	}
 
 }
