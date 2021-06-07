4f429ef677293815e47d04a082c9979c94cafc99
Dhruv <dhruv.hansu@gmail.com>
2021-06-07 22:05:13 +0530

    Pratik change

--- src/main/java/com/FabRoadies/entity/Admin.java
+++ src/main/java/com/FabRoadies/entity/Admin.java
@@ -1,17 +1,5 @@
 package com.FabRoadies.entity;
 
-<<<<<<< HEAD:src/main/java/com/FabRoadies/entity/User.java
-
-
-import javax.persistence.*;
-import java.util.List;
-
-@Entity
-@Table(name = "users")
-//@JsonIgnoreProperties({"tickets"})
-public class User {
-    @Id
-=======
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -23,9 +11,8 @@ import javax.persistence.Table;
 @Table(name = "admin")
 public class Admin {
 	@Id
->>>>>>> f5efda80272d03354a0fa60834abf8e040a81568:src/main/java/com/FabRoadies/entity/Admin.java
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    private Long id;
 
     @Column(nullable = false, unique = true)
     private String email;
@@ -33,47 +20,8 @@ public class Admin {
     private String name;
     @Column(nullable = false)
     private String password;
-<<<<<<< HEAD:src/main/java/com/FabRoadies/entity/User.java
-    private String name;
-=======
->>>>>>> f5efda80272d03354a0fa60834abf8e040a81568:src/main/java/com/FabRoadies/entity/Admin.java
     private String phoneNumber;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
 
-<<<<<<< HEAD:src/main/java/com/FabRoadies/entity/User.java
-    
-
-
-=======
     public String getEmail() {
         return email;
     }
@@ -112,5 +60,4 @@ public class Admin {
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
->>>>>>> f5efda80272d03354a0fa60834abf8e040a81568:src/main/java/com/FabRoadies/entity/Admin.java
 }
---
+++ src/main/java/com/FabRoadies/entity/User.java
@@ -0,0 +1,52 @@
+package com.FabRoadies.entity;
+
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+    private String name;
+    private String phoneNumber;
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
