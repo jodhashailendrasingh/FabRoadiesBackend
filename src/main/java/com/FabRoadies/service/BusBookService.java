a9cfc91f237172755ab24495ea034014028ccb00
Dhruv <dhruv.hansu@gmail.com>
2021-06-10 19:37:39 +0530

    git problem

--- src/main/java/com/FabRoadies/service/BusBookService.java
+++ src/main/java/com/FabRoadies/service/BusBookService.java
@@ -9,53 +9,59 @@ import com.FabRoadies.dto.BookingRequest;
 import com.FabRoadies.entity.Bus;
 import com.FabRoadies.entity.Passenger;
 import com.FabRoadies.entity.Ticket;
+import com.FabRoadies.entity.User;
 import com.FabRoadies.repo.BusRepository;
 import com.FabRoadies.repo.PassengerRepo;
 import com.FabRoadies.repo.TicketRepo;
-
+import com.FabRoadies.repo.UserRepo;
 
 @Service
 public class BusBookService {
 
-    @Autowired
-    private BusRepository busRepository;
-    @Autowired
-    private PassengerRepo passengerRepository;
+	@Autowired
+	private BusRepository busRepository;
+	@Autowired
+	private PassengerRepo passengerRepository;
 
-    @Autowired
-    private TicketRepo reservationRepository;
+	@Autowired
+	private TicketRepo reservationRepository;
+	@Autowired
+	private UserRepo userRepository;
+
 	public Ticket bookBus(BookingRequest reservationRequest) {
-		
-      
-        String busno=reservationRequest.getBusno();
-     
-		Optional<Bus> busOptional=busRepository.findById(busno);
-        if (!busOptional.isPresent()) {
-            throw new RuntimeException();
-        }
-      
-        Bus bus=busOptional.get();
-        Passenger passenger=new Passenger();
-        passenger.setSeatno(reservationRequest.getSeatno());
-        passenger.setName(reservationRequest.getName());
-        passenger.setGender(reservationRequest.isGender());
-        passenger.setAge(reservationRequest.getAge());
 
-        passengerRepository.save(passenger);
-       
+		///////// Ticket add///////////
 
-        Ticket reservation=new Ticket();
-         reservation.setBus(bus);
-//         reservation.setUser();
-         
-         
-//        reservation.setPassenger(passenger);
-        Ticket savedReservation = reservationRepository.save(reservation);
-       
+		String busno = reservationRequest.getBusno();
+		int userid = reservationRequest.getUserid();
 
+		Optional<Bus> busOptional = busRepository.findById(busno);
 
-      
-        return savedReservation;
-//return null;
-    }
+		if (!busOptional.isPresent()) {
+			throw new RuntimeException();
+		}
+
+		Bus bus = busOptional.get();
+		Optional<User> userOptional = userRepository.findById(userid);
+		User user = userOptional.get();
+		Ticket reservation = new Ticket();
+		reservation.setBus(bus);
+		reservation.setUser(user);
+		Ticket savedReservation = reservationRepository.save(reservation);
+
+		///////// Passenger add///////////
+
+		Passenger passenger = new Passenger();
+		passenger.setSeatno(reservationRequest.getSeatno());
+		passenger.setName(reservationRequest.getName());
+		passenger.setGender(reservationRequest.isGender());
+		passenger.setAge(reservationRequest.getAge());
+		// passenger.setTicket(reservation);
+		passenger.setTicket(reservationRepository.getById((long) 39));
+		passengerRepository.save(passenger);
+
+		return savedReservation;
+
+		// return null;
+	}
 }
